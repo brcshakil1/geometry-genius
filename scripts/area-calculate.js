ca5a0ceac3232d9a5bcfae04d9d4ec0864f931aa
brcshakil1 <brcshakil72@gmail.com>
2023-08-18 12:09:09 +0600

    calculating function Ellipse and Parallelogram area done

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -1,40 +1,63 @@
 function calculateTriangleArea() {
     // get triangle base value
-    const baseField = document.getElementById('triangle-base');
-    const baseValue = baseField.value;
-    const base = parseFloat(baseValue);
-    console.log(base);
+    const base = inputValue('triangle-base');
 
     // get triangle base value
-    const heightFiled = document.getElementById('triangle-height');
-    const heightValue = heightFiled.value;
-    const height = parseFloat(heightValue);
-    console.log(height);
+    const height = inputValue('triangle-height');
 
     const area = 0.5 * base * height;
-    console.log(area)
-
     // show triangle area
-    const triangleAreaField = document.getElementById('triangle-area');
-    triangleAreaField.innerHTML = `${area} cm<sup>2</sup>`
+    setElementInnerText('triangle-area', area);
 }
 
 function calculateRectangleArea() {
     // get triangle base value
-    const widthField = document.getElementById('rectangle-width');
-    const widthValue = widthField.value;
-    const width = parseFloat(widthValue);
-    console.log(width);
+    const width = inputValue('rectangle-width');
 
     // get triangle base value
-    const lengthFiled = document.getElementById('rectangle-length');
-    const lengthValue = lengthFiled.value;
-    const length = parseFloat(lengthValue);
-    console.log(length);
+    const length = inputValue('rectangle-length');
 
     const area = width * length;
-    console.log(area)
 
-    const rectangleAreaField = document.getElementById('rectangle-area');
-    rectangleAreaField.innerHTML = `${area} cm<sup>2</sup>`
+    // show rectangle area
+    setElementInnerText('rectangle-area', area);
+}
+
+function calculateParallelogramArea () {
+    // get parallelogram base value
+    const base = inputValue('parallelogram-base');
+
+    // get parallelogram height value
+    const height = inputValue('parallelogram-height');
+
+    // get triangle area
+    const area = base * height;
+    
+    // show parallelogram area
+    setElementInnerText('parallelogram-area', area);
+}
+
+function calculateEllipseArea() {
+    const majorRadius = inputValue('ellipse-major-radius');
+    const minorRadius = inputValue('ellipse-minor-radius');
+    const area = 3.14 * majorRadius  * minorRadius;
+    setElementInnerText('ellipse-area',area)
+}
+
+// reusable function
+
+function inputValue(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputValue = inputField.value;
+    const value = parseFloat(inputValue);
+    inputField.value = '';
+    return value
+
+}
+
+// reusable span, p, div etc text
+
+function setElementInnerText(ElementId, area) {
+    const element = document.getElementById(ElementId);
+    element.innerHTML = `${area} cm<sup>2</sup>`;
 }
